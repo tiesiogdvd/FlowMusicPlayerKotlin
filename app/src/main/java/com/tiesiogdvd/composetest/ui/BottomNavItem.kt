6b86d4dfce589b82c390bfacfea046580894566f
tiesiogdvd <deivukelis@gmail.com>
2023-02-24 05:16:21 +0200

    Added BottomNavItems object to reduce clutter in BottomNavigation composables

--- app/src/main/java/com/tiesiogdvd/composetest/ui/BottomNavItem.kt
+++
@@ -1,7 +0,0 @@
-package com.tiesiogdvd.composetest.ui
-
-data class BottomNavItem(
-    val name:String,
-    val route:String,
-    val icon: Int
-)
---
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/bottomNavBar/BottomNavItem.kt
@@ -0,0 +1,37 @@
+package com.tiesiogdvd.composetest.ui.bottomNavBar
+
+import com.tiesiogdvd.composetest.R
+
+data class BottomNavItem(
+    val name:String,
+    val route:String,
+    val icon: Int
+)
+
+object BottomNavItems {
+    val BottomNavItems = listOf(
+        BottomNavItem(
+        name = "Library",
+        route = "library",
+        icon = R.drawable.ic_action_navbar_library2
+        ),
+        BottomNavItem(
+        name = "Equalizer",
+        route = "equalizer",
+        icon = R.drawable.ic_action_navbar_equalizer1
+        ),
+        BottomNavItem(
+        name = "Settings",
+        route = "settings",
+        icon = R.drawable.ic_action_navbar_settings
+        )
+    )
+}
+
+object MusicPlayerItem{
+    val item = BottomNavItem(
+        name = "Player",
+        route = "player",
+        icon = R.drawable.ic_action_play
+    )
+}
