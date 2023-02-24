7e03c6e0f7101bfa6868c30f8ff8ecf8bf03b51c
tiesiogdvd <deivukelis@gmail.com>
2023-02-24 05:35:06 +0200

    Added isPlaying, isNavbarVisible, isSongbarVisible state values

--- app/src/main/java/com/tiesiogdvd/composetest/ui/bottomNavBar/NavbarViewModel.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/bottomNavBar/NavbarViewModel.kt
@@ -1,42 +1,33 @@
-package com.tiesiogdvd.composetest.ui
+package com.tiesiogdvd.composetest.ui.bottomNavBar
 
-import android.media.session.PlaybackState
-import android.os.Bundle
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
-import androidx.media3.session.MediaBrowser
 import com.tiesiogdvd.composetest.data.PreferencesManager
 import com.tiesiogdvd.playlistssongstest.data.MusicDao
+import com.tiesiogdvd.composetest.service.ServiceConnector
 import com.tiesiogdvd.playlistssongstest.data.Song
-import com.tiesiogdvd.service.ServiceConnector
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
 @HiltViewModel
-class MainViewModel @Inject constructor(
+class NavbarViewModel @Inject constructor(
     private val preferencesManager: PreferencesManager,
     private val musicDao: MusicDao,
     private val serviceConnector: ServiceConnector,
+    private val navbarController: NavbarController,
 ) : ViewModel() {
     val currentSong = serviceConnector.currentSong
+    val isPlaying = serviceConnector.playbackState
+    val isNavbarVisible = navbarController.navbarEnabled
+    val isSongbarVisible = navbarController.songbarEnabled
 
 
-    fun playSource(){
-       serviceConnector.controller?.seekToNext()
+    val sourcePreferencesFlow = preferencesManager.currentSongFlow
+    val currentSource = sourcePreferencesFlow.flatMapLatest {
+        musicDao.getSongFromId(it.currentSongID)!!
     }
-
-    val sourcePreferencesFlow = preferencesManager.preferencesFlow
-    val sourceFlow = sourcePreferencesFlow.flatMapLatest { musicDao.getSongFromId(it.currentSongID)!! }
-
-    val currentSource = sourceFlow
 
 
     fun changePlaybackState(){
@@ -45,10 +36,6 @@ class MainViewModel @Inject constructor(
             false -> {serviceConnector.controller?.play()}
             else -> {serviceConnector.controller?.prepare()}
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
     }
 
 }
