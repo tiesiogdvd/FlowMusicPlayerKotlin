d5233a06a5423eb8bf86dc8094ca6b2f9d650c99
tiesiogdvd <deivukelis@gmail.com>
2023-02-24 05:11:45 +0200

    Created ViewModel for AddToPlaylist dialog

---
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/addToPlaylistDialog/AddToPlaylistDialogViewModel.kt
@@ -0,0 +1,73 @@
+package com.tiesiogdvd.composetest.ui.addToPlaylistDialog
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tiesiogdvd.playlistssongstest.data.MusicDao
+import com.tiesiogdvd.playlistssongstest.data.Playlist
+import com.tiesiogdvd.playlistssongstest.data.PlaylistWithSongs
+import com.tiesiogdvd.playlistssongstest.data.Song
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddToPlaylistDialogViewModel @Inject constructor(
+    val musicDao: MusicDao,
+): ViewModel(){
+    val playlists = musicDao.getPlaylistsWithSongs(false)
+    var isAddPlaylistDialogEnabled = mutableStateOf(false)
+
+
+    suspend fun isSongInPlaylist(playlist:Playlist,song: Song):Boolean{
+        return musicDao.songExistsInPlaylist(song.songPath, playlist.id)
+    }
+
+    fun toggleAddPlaylistDialog() {
+        isAddPlaylistDialogEnabled.value = !isAddPlaylistDialogEnabled.value
+    }
+
+    fun playlistExists(playlistName:String, playlistsWithSongs: List<PlaylistWithSongs>):Boolean{
+        for(playlistWithSongs in playlistsWithSongs){
+            if(playlistName==playlistWithSongs.playlist.playlistName){
+                return true
+            }
+        }
+        return false
+    }
+
+    fun addPlaylist(playlistName: String, songs:Map<Int,Song>?){
+        println("ADDING PLAYLIST")
+        viewModelScope.launch {
+            musicDao.insertPlaylist(Playlist(playlistName))
+
+            if(songs!=null){
+                val songList:ArrayList<Song> = ArrayList()
+                for(song in songs){
+                    val tempSong = Song(
+                        songName = song.value.songName,
+                        songPath = song.value.songPath,
+                        folder = song.value.folder,
+                        length = song.value.length,
+                        isHidden = song.value.isHidden,
+                        hasBitmap = song.value.hasBitmap,
+                        isBitmapCached = song.value.isBitmapCached,
+                        inFavorites = song.value.inFavorites,
+                        inAllSongs = song.value.inAllSongs,
+                        songArtist = song.value.songArtist,
+                        albumArtist = song.value.albumArtist,
+                        album = song.value.album,
+                        genre = song.value.genre,
+                        trackNumber = song.value.trackNumber,
+                        year = song.value.year,
+
+                        playlistId = musicDao.getPlaylist(playlistName).id)
+                    songList.add(tempSong)
+                }
+                musicDao.insertSongsToPlaylist(songList)
+            }
+        }
+    }
+
+
+}
