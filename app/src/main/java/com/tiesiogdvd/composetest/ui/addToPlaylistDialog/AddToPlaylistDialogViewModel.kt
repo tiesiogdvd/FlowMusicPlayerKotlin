98c7496a6733d298a0d302fd330041167412d90a
tiesiogdvd <deivukelis@gmail.com>
2023-03-02 01:06:18 +0200

    Testing adding and removing lists of specific items to specific playlists

--- app/src/main/java/com/tiesiogdvd/composetest/ui/addToPlaylistDialog/AddToPlaylistDialogViewModel.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/addToPlaylistDialog/AddToPlaylistDialogViewModel.kt
@@ -1,5 +1,6 @@
 package com.tiesiogdvd.composetest.ui.addToPlaylistDialog
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,6 +9,7 @@ import com.tiesiogdvd.playlistssongstest.data.Playlist
 import com.tiesiogdvd.playlistssongstest.data.PlaylistWithSongs
 import com.tiesiogdvd.playlistssongstest.data.Song
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -36,36 +38,76 @@ class AddToPlaylistDialogViewModel @Inject constructor(
         return false
     }
 
+    fun toggleAddStatus(playlistId:Int, songs:Map<Int,Song>?, isSelected: Boolean){
+        viewModelScope.launch {
+            if(!isSelected){
+                addSongsToPlaylist(playlistId, songs)
+            }else{
+                removeSongsFromPlaylist(playlistId,songs)
+            }
+        }
+    }
+
     fun addPlaylist(playlistName: String, songs:Map<Int,Song>?){
         println("ADDING PLAYLIST")
         viewModelScope.launch {
             musicDao.insertPlaylist(Playlist(playlistName))
+            val playlistId =  musicDao.getPlaylist(playlistName).id
+            addSongsToPlaylist(playlistId,songs)
+        }
+    }
 
-            if(songs!=null){
-                val songList:ArrayList<Song> = ArrayList()
-                for(song in songs){
-                    val tempSong = Song(
-                        songName = song.value.songName,
-                        songPath = song.value.songPath,
-                        folder = song.value.folder,
-                        length = song.value.length,
-                        isHidden = song.value.isHidden,
-                        hasBitmap = song.value.hasBitmap,
-                        isBitmapCached = song.value.isBitmapCached,
-                        inFavorites = song.value.inFavorites,
-                        inAllSongs = song.value.inAllSongs,
-                        songArtist = song.value.songArtist,
-                        albumArtist = song.value.albumArtist,
-                        album = song.value.album,
-                        genre = song.value.genre,
-                        trackNumber = song.value.trackNumber,
-                        year = song.value.year,
-
-                        playlistId = musicDao.getPlaylist(playlistName).id)
-                    songList.add(tempSong)
-                }
-                musicDao.insertSongsToPlaylist(songList)
+    suspend fun addSongsToPlaylist(playlistId: Int, songs:Map<Int,Song>?){
+        if(songs!=null){
+            val songList:ArrayList<Song> = ArrayList()
+            for(song in songs){
+                val tempSong = Song(
+                    songName = song.value.songName,
+                    songPath = song.value.songPath,
+                    folder = song.value.folder,
+                    length = song.value.length,
+                    isHidden = song.value.isHidden,
+                    hasBitmap = song.value.hasBitmap,
+                    isBitmapCached = song.value.isBitmapCached,
+                    inFavorites = song.value.inFavorites,
+                    inAllSongs = song.value.inAllSongs,
+                    songArtist = song.value.songArtist,
+                    albumArtist = song.value.albumArtist,
+                    album = song.value.album,
+                    genre = song.value.genre,
+                    trackNumber = song.value.trackNumber,
+                    year = song.value.year,
+                    playlistId = playlistId)
+                songList.add(tempSong)
             }
+            musicDao.insertSongsToPlaylist(songList)
+        }
+    }
+
+    suspend fun removeSongsFromPlaylist(playlistId: Int, songs: Map<Int, Song>?){
+        if(songs!=null){
+            val songList:ArrayList<Song> = ArrayList()
+            for(song in songs){
+                val tempSong = Song(
+                    songName = song.value.songName,
+                    songPath = song.value.songPath,
+                    folder = song.value.folder,
+                    length = song.value.length,
+                    isHidden = song.value.isHidden,
+                    hasBitmap = song.value.hasBitmap,
+                    isBitmapCached = song.value.isBitmapCached,
+                    inFavorites = song.value.inFavorites,
+                    inAllSongs = song.value.inAllSongs,
+                    songArtist = song.value.songArtist,
+                    albumArtist = song.value.albumArtist,
+                    album = song.value.album,
+                    genre = song.value.genre,
+                    trackNumber = song.value.trackNumber,
+                    year = song.value.year,
+                    playlistId = playlistId)
+                songList.add(tempSong)
+            }
+            musicDao.removeSongs(songList)
         }
     }
 
