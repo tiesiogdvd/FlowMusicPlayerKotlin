f0ccc083177de419eadbf17e7298179fa23e0ff4
tiesiogdvd <deivukelis@gmail.com>
2023-03-03 15:55:31 +0200

    Added enum class NavRoutes

--- app/src/main/java/com/tiesiogdvd/composetest/ui/bottomNavBar/BottomNavItem.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/bottomNavBar/BottomNavItem.kt
@@ -2,6 +2,14 @@ package com.tiesiogdvd.composetest.ui.bottomNavBar
 
 import com.tiesiogdvd.composetest.R
 
+enum class NavRoutes{
+    LIBRARY,
+    EQUALIZER,
+    SETTINGS,
+    YT_DOWNLOAD,
+    PLAYER
+}
+
 data class BottomNavItem(
     val name:String,
     val route:String,
@@ -12,22 +20,22 @@ object BottomNavItems {
     val BottomNavItems = listOf(
         BottomNavItem(
         name = "Library",
-        route = "library",
+        route = NavRoutes.LIBRARY.name,
         icon = R.drawable.ic_action_navbar_library2
         ),
         BottomNavItem(
         name = "Equalizer",
-        route = "equalizer",
+        route = NavRoutes.EQUALIZER.name,
         icon = R.drawable.ic_action_navbar_equalizer1
         ),
         BottomNavItem(
         name = "Settings",
-        route = "settings",
+        route = NavRoutes.SETTINGS.name,
         icon = R.drawable.ic_action_navbar_settings
         ),
         BottomNavItem(
         name = "YT Download",
-        route = "yt_download",
+        route = NavRoutes.YT_DOWNLOAD.name,
         icon = R.drawable.ic_action_note_no_alpha
     )
     )
@@ -36,7 +44,7 @@ object BottomNavItems {
 object MusicPlayerItem{
     val item = BottomNavItem(
         name = "Player",
-        route = "player",
+        route = NavRoutes.PLAYER.name,
         icon = R.drawable.ic_action_play
     )
 }
