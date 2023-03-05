e68acf344cfd876d9dd2181ff8b7951fa58df69e
tiesiogdvd <deivukelis@gmail.com>
2023-03-05 18:12:42 +0200

    Created Transitions object to hold transition animations

---
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/theme/Transitions.kt
@@ -0,0 +1,26 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
+package com.tiesiogdvd.composetest.ui.theme
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.TransformOrigin
+
+object Transitions {
+
+    val enter2 = slideInVertically(
+        initialOffsetY = { -40 }
+    ) + expandVertically(
+        expandFrom = Alignment.CenterVertically
+    ) + scaleIn(
+        // Animate scale from 0f to 1f using the top center as the pivot point.
+        transformOrigin = TransformOrigin(
+            1.5f, 0f)
+    ) + fadeIn(initialAlpha = 0f)
+
+    val enter = scaleIn(tween(300, easing = EaseInOut),initialScale = 1.15f) + fadeIn(tween(300, easing = EaseInOut))
+
+
+    val exit = fadeOut(tween(300, easing = EaseInOut)) + scaleOut(tween(300, easing = EaseInOut),targetScale = 1.15f)
+}
