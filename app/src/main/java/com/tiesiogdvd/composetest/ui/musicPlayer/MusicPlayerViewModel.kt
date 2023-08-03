7550d7c46d860d8a81426cae1b33d07ca11a7680
tiesiogdvd <deivukelis@gmail.com>
2023-08-03 15:43:08 +0200

    Separated amplituda from this viewmodel, added methods to work with viewpager

--- app/src/main/java/com/tiesiogdvd/composetest/ui/musicPlayer/MusicPlayerViewModel.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/musicPlayer/MusicPlayerViewModel.kt
@@ -1,88 +1,148 @@
 package com.tiesiogdvd.composetest.ui.musicPlayer
 
 import android.app.Application
+import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ShuffleOrder
 import com.tiesiogdvd.composetest.data.PreferencesManager
+import com.tiesiogdvd.composetest.data.settings.SettingsManager
+import com.tiesiogdvd.composetest.service.MusicSource
 import com.tiesiogdvd.composetest.service.ServiceConnector
+import com.tiesiogdvd.composetest.ui.libraryPlaylist.BitmapLoader
+import com.tiesiogdvd.composetest.util.AudioAmplitudes
 import com.tiesiogdvd.composetest.util.convertListToArray
 import com.tiesiogdvd.playlistssongstest.data.MusicDao
 import com.tiesiogdvd.playlistssongstest.data.Song
+import dagger.Provides
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import linc.com.amplituda.*
-import javax.inject.Inject
 
-@HiltViewModel
+import fetchLyrics4
+import fetchLyricsWithKtor
+import fetchLyricsWithKtor2
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import linc.com.amplituda.*
+import linc.com.amplituda.exceptions.AmplitudaException
+import okhttp3.internal.wait
+import java.io.File
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@OptIn(ExperimentalFoundationApi::class)
+@UnstableApi @HiltViewModel
 class MusicPlayerViewModel @Inject constructor(
     private val serviceConnector: ServiceConnector,
-    preferencesManager: PreferencesManager,
-    private val musicDao: MusicDao,
-    val context:Application
+    private val musicSource: MusicSource,
+    private val settingsManager: SettingsManager,
+    private val preferencesManager: PreferencesManager,
 
     ):ViewModel(){
-    val currentSong = serviceConnector.currentSong
     val currentPosition = MutableStateFlow(0)
     val isPlaying = serviceConnector.playbackState
-    val controller = serviceConnector.controller
 
-    var amplituda = MutableStateFlow<IntArray?>(null)
-    var songDuration = MutableStateFlow<Long>(0)
+    val currentSource = serviceConnector.currentSource
 
-    val sourcePreferencesFlow = preferencesManager.currentSongFlow
-    val currentSource = sourcePreferencesFlow.flatMapLatest {
-        musicDao.getSongFromId(it.currentSongID)!!
+    val songsList = musicSource.sourcePlaylist
+
+    val songsList2 = serviceConnector.curSongList
+
+    val shuffleList = serviceConnector.curShuffleList
+
+    val curSongIndex = serviceConnector.curSongIndex
+
+    val curSongLyrics = serviceConnector.currentSongLyrics
+
+    val playerScreenSettings = settingsManager.playerSettingsFlow
+
+    val shuffleStatus = serviceConnector.shuffleStatus
+
+    @OptIn(ExperimentalFoundationApi::class)
+    var pagerState = PagerState(initialPage = curSongIndex.value?: indexFromMusicSource()?:0, initialPageOffsetFraction = 0f)
+
+    val sourceSettings = preferencesManager.currentPlayModeFlow
+
+    var bitmap = serviceConnector.bitmap
+
+    var songDelayJob: Job? = null
+    private val songDelayScope = CoroutineScope(Dispatchers.IO)
+    fun indexFromMusicSource():Int? = musicSource.currentSongIndex
+
+    fun seekToPosition(position: Long){
+        serviceConnector.seekTo(position)
+    }
+    fun getSongIndex(song:Song?):Int? = musicSource.itemIndexById(song?.id)
+
+
+    fun changePlaybackState(){
+        serviceConnector.changePlaybackState()
     }
 
-    val amplitudaProcess = Amplituda(context)
+    fun playNext(){
+        serviceConnector.playNext()
+    }
 
-    val amplitudaProgressListener = object: AmplitudaProgressListener() {
-        override fun onProgress(operation: ProgressOperation?, progress: Int) {}
-        override fun onStartProgress() { super.onStartProgress() }
-        override fun onStopProgress() { super.onStopProgress()
+    fun playPrev(){
+        serviceConnector.playPrev()
+    }
+
+    fun playNextDelay(millis: Long){
+        songDelayJob?.cancel()
+        songDelayJob = songDelayScope.launch {
+            delay(millis)
+            withContext(Dispatchers.Main){
+                serviceConnector.playNext()
+            }
+        }
+    }
+    fun playPrevDelay(millis: Long){
+        songDelayJob?.cancel()
+        songDelayJob = songDelayScope.launch {
+            delay(millis)
+            withContext(Dispatchers.Main){
+                serviceConnector.playPrev()
+                //serviceConnector.controller.getshu
+            }
+
         }
     }
 
-    init {
-        currentSource.asLiveData().observeForever{
-            viewModelScope.launch{
-                amplituda.value=null
-                if(it!=null && it.songName!=null){
-                    getAmplituda(it)
+    fun playIndexDelay(millis: Long){
+        songDelayJob?.cancel()
+        songDelayJob = songDelayScope.launch {
+            delay(millis)
+            withContext(Dispatchers.Main){
+                if(curSongIndex.value!=null){
+                 //   serviceConnector.controller?.seekTo(curSongIndex.value!!, 0)
                 }
             }
         }
     }
 
-    suspend fun getAmplituda(song: Song) = withContext(Dispatchers.Default){
-        val amplituda = amplitudaProcess.processAudio(song.songPath, Compress.withParams(Compress.AVERAGE,1), Cache.withParams(Cache.REUSE), amplitudaProgressListener).get().amplitudesAsList()
-        if(currentSource.first().songName==song.songName){
-            this@MusicPlayerViewModel.amplituda.value = null
-            this@MusicPlayerViewModel.amplituda.value = convertListToArray(amplituda)
-            this@MusicPlayerViewModel.songDuration.value = song.length
+    fun playPage(index:Int, song: Song){
+        if(shuffleStatus.value == false){
+            if (pagerState.currentPage != curSongIndex.value || getSongIndex(song) != curSongIndex.value) {
+                if(index!=curSongIndex.value){
+                    serviceConnector.controller?.seekTo(index, 0)
+                }
+            }
+        }else{
+            if (pagerState.currentPage != shuffleList.value.indexOf(curSongIndex.value)  || shuffleList.value.indexOf(getSongIndex(song)) != shuffleList.value.indexOf(curSongIndex.value) ) {
+                serviceConnector.controller?.seekTo(shuffleList.value.get(index), 0)
+            }
         }
     }
+    fun getPlaybackState():Int = serviceConnector.controller?.playbackState?: ExoPlayer.STATE_IDLE
 
-    fun changePlaybackState(){
-        when(serviceConnector.controller?.isPlaying){
-            true -> {serviceConnector.controller?.pause()}
-            false -> {serviceConnector.controller?.play()}
-            else -> {serviceConnector.controller?.prepare()}
-        }
-    }
+    fun getCurrentPosition():Long = serviceConnector.getPosition()
 
-    fun playNext(){
-        serviceConnector.controller?.seekToNext()
-    }
-
-    fun playPrev(){
-        serviceConnector.controller?.seekToPrevious()
-    }
 
 }
