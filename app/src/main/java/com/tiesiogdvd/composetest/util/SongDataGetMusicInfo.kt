9326453f736bde66987d7a5f37ed58e5ab106f35
tiesiogdvd <deivukelis@gmail.com>
2023-09-11 10:15:13 +0200

    Modified music retrieval to support cross reference

--- app/src/main/java/com/tiesiogdvd/composetest/util/SongDataGetMusicInfo.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/util/SongDataGetMusicInfo.kt
@@ -10,9 +10,10 @@ import java.io.File
 
 object SongDataGetMusicInfo{
     suspend fun getMusicInfo(musicDao: MusicDao, context: Application) {
-        val songList: ArrayList<Song> = ArrayList()
+        val songListNotInDatabase: ArrayList<Song> = ArrayList()
+        val songList: List<Long>
         if(!musicDao.playlistExists("All Songs")){
-            musicDao.insertPlaylist(Playlist("All Songs"))
+            musicDao.insertPlaylist(Playlist("All Songs", isHidden = true))
         }
         val playlist = musicDao.getPlaylist("All Songs")
 
@@ -28,24 +29,38 @@ object SongDataGetMusicInfo{
                 )
                 val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
                 if (isMusic != 0) {
-                    if(!musicDao.songExistsInPlaylist(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)), playlist.id)){
-                        songList.add(Song(
-                            songPath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)),
-                            songName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)),
-                            folder = File(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))).parent,
-                            length = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)),
-                            songArtist = if(artist=="<unknown>"){null}else{artist},
-                            albumArtist = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ARTIST))}else{null},
-                            album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)),
-                            genre = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.GENRE))}else{null},
-                            trackNumber = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TRACK)), //Might be CD TRACK NUMBER
-                            year = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.YEAR)),
-                            playlistId = playlist.id))
+                    val songPath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
+                    val song = Song(
+                        songPath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)),
+                        songName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)),
+                        folder = File(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))).parent,
+                        length = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)),
+                        songArtist = if(artist=="<unknown>"){null}else{artist},
+                        albumArtist = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ARTIST))}else{null},
+                        album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)),
+                        genre = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.GENRE))}else{null},
+                        trackNumber = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TRACK)), //Might be CD TRACK NUMBER
+                        year = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.YEAR))
+                    )
+                    if(musicDao.songExists(songPath) == false){
+                        songListNotInDatabase.add(song)
                     }
+/*                    else{
+                        println(song.songName)
+                        val id = musicDao.getSong(songPath)?.id
+                        if(id!=null && musicDao.songExistsInPlaylist(songId = id, playlistId = playlist.id) == false){
+                            val songItem = musicDao.getSong(songPath)
+                            if(songItem!=null){
+                                songList.add(songItem)
+                            }
+                        }
+                    }*/
                 }
             }
         }
         cursor?.close()
-        musicDao.insertSongsList(songList)
+        songList = musicDao.insertSongs(songListNotInDatabase)
+        songList.forEach{ println(it)}
+        musicDao.insertSongsToPlaylist(songIds = songList, playlistId = playlist.id)
     }
 }
