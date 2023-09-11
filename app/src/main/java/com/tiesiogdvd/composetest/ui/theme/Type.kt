7ce1a5abd67e9e4f36d5bb029987a54b109fb3d1
tiesiogdvd <deivukelis@gmail.com>
2023-09-11 10:15:42 +0200

    Added Jost font type

--- app/src/main/java/com/tiesiogdvd/composetest/ui/theme/Type.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.tiesiogdvd.composetest.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tiesiogdvd.composetest.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +28,12 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+
+val Jost = FontFamily(
+    Font(R.font.jost_bolditalic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.jost_mediumitalic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.jost_regular, weight = FontWeight.Normal, style = FontStyle.Italic)
+
 )
