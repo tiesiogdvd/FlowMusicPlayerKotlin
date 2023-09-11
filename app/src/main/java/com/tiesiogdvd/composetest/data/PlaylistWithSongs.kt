360d3d54b481da7d80a3d464ff3dc66d5c13b889
tiesiogdvd <deivukelis@gmail.com>
2023-09-11 09:44:31 +0200

    Implemented PlaylistSongCrossRef to avoid duplicate Song entities for each playlist, added methods to check playlist data

--- app/src/main/java/com/tiesiogdvd/composetest/data/PlaylistWithSongs.kt
+++ app/src/main/java/com/tiesiogdvd/composetest/data/PlaylistWithSongs.kt
@@ -2,15 +2,41 @@ package com.tiesiogdvd.playlistssongstest.data
 
 import android.os.Parcelable
 import androidx.room.Embedded
+import androidx.room.Junction
 import androidx.room.Relation
+import com.tiesiogdvd.composetest.data.PlaylistSongCrossRef
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class PlaylistWithSongs(
-    @Embedded val playlist:Playlist,
+    @Embedded val playlist: Playlist,
     @Relation(
+        entity = Song::class,
         parentColumn = "id",
-        entityColumn = "playlistId"
+        entityColumn = "id",
+        associateBy = Junction(
+            value = PlaylistSongCrossRef::class,
+            parentColumn = "playlistId",
+            entityColumn = "songId"
+        )
     )
     val songs: List<Song>
-): Parcelable
+): Parcelable{
+    fun containsSong(song: Song): Boolean {
+        return songs.contains(song)
+    }
+
+    fun containsAllSongs(songsList: List<Song>): Boolean {
+        return songs.containsAll(songsList)
+    }
+
+    fun containsSongsFromMap(songMap: Map<Int, Song>): Boolean {
+        return songs.map { it.id }.containsAll(songMap.keys)
+    }
+
+}
+
+
+object EmptyPlaylist{
+    val playlistWithSongs = PlaylistWithSongs(playlist = Playlist("dummy", id = -1), songs = emptyList())
+}
